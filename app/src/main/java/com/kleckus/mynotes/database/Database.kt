4acf9e2255c5b9fed96d63692e671d977325cf87
Willian Mota Oliveira <wmoliveira1000@gmail.com>
2020-10-02 13:24:31 -0300

    Save and load state of the database is now created

--- app/src/main/java/com/kleckus/mynotes/database/Database.kt
+++ app/src/main/java/com/kleckus/mynotes/database/Database.kt
@@ -1,10 +1,45 @@
 package com.kleckus.mynotes.database
 
 import android.content.Context
+import com.kleckus.mynotes.system.*
 import io.paperdb.Paper
+
+enum class DB_KEYS(val key : String){
+    NOTE("note-key"),
+    BOOK("book-key")
+}
+enum class BOOKS(val key : String){
+    MAIN_BOOK("main-book-key")
+}
 
 class Database{
     companion object{
         fun initializeDatabase(context: Context){ Paper.init(context) }
+
+        fun loadState() : Promise<Boolean>{
+            val ret = Promise<Boolean>()
+            var success = false
+            Async{
+                MyNotesSystem.noteList = Paper.book(BOOKS.MAIN_BOOK.key).read<MutableList<Note>>(DB_KEYS.NOTE.key)
+                MyNotesSystem.bookList = Paper.book(BOOKS.MAIN_BOOK.key).read<MutableList<Book>>(DB_KEYS.BOOK.key)
+            }.andThen {
+                success = true
+                ret.complete(success)
+            }
+            return ret
+        }
+
+        fun saveState() : Promise<Boolean>{
+            val ret = Promise<Boolean>()
+            var success = false
+            Async{
+                Paper.book(BOOKS.MAIN_BOOK.key).write(DB_KEYS.NOTE.key, MyNotesSystem.noteList)
+                Paper.book(BOOKS.MAIN_BOOK.key).write(DB_KEYS.BOOK.key, MyNotesSystem.bookList)
+            }.andThen {
+                success = true
+                ret.complete(success)
+            }
+            return ret
+        }
     }
 }
---
+++ app/src/main/java/com/kleckus/mynotes/ui/Models.kt
@@ -0,0 +1,21 @@
+package com.kleckus.mynotes.ui
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MainAdapter : RecyclerView.Adapter<MainAdapter.VH>(){
+    class VH (itemView : View) : RecyclerView.ViewHolder(itemView) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
