7b2c5c58991f7a8f1850274afc335508c649fb68
Willian Mota Oliveira <wmoliveira1000@gmail.com>
2020-10-05 14:10:30 -0300

    Created the dialog file for the password creation and validation

--- app/src/main/java/com/kleckus/mynotes/ui/CVPasswordDialog.kt
+++ app/src/main/java/com/kleckus/mynotes/ui/CVPasswordDialog.kt
@@ -10,10 +10,10 @@ import kotlinx.android.synthetic.main.create_validade_password_dialog_layout.vie
 
 class CVPasswordDialog(private val isCreating : Boolean)  : DialogFragment() {
 
+    var onFinish : (password : String) -> Unit = {}
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog
     {
-        var onFinish : (password : String) -> Unit = {}
-
         val builder = AlertDialog.Builder(activity)
         val inflater = activity?.layoutInflater
         val view = inflater!!.inflate(R.layout.create_validade_password_dialog_layout, null)
@@ -21,7 +21,29 @@ class CVPasswordDialog(private val isCreating : Boolean)  : DialogFragment() {
         if(isCreating) view.validatingPasswordView.visibility = View.GONE
         else view.creatingPasswordView.visibility = View.GONE
 
+        view.doneButton.setOnClickListener {
+            if(isCreating) {
+                val createdPassword = view.inputCreatePassword.text.toString()
+                val passwordConfirmation = view.inputConfirmPassword.text.toString()
+                onCreating(createdPassword, passwordConfirmation)
+            }
+            else{
+                val password = view.inputValidatePassword.text.toString()
+                onValidating(password)
+            }
+        }
+
         builder.setView(view)
         return builder.create()
+    }
+
+    private fun onCreating(passwordOne: String, passwordTwo: String){
+        if(passwordOne == passwordTwo){
+            onFinish(passwordOne)
+        }
+    }
+
+    private fun onValidating(password : String){
+        onFinish(password)
     }
 }
